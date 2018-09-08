2a0b7be0d88dc19eaa9b12a045b841cfedf304ec
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-09-08 16:08:36 -0500

    finished parameter filter expression building

--- R/swat2012_parameter_constrain.R
+++ R/swat2012_parameter_constrain.R
@@ -4,6 +4,7 @@ library(pasta)
 a <- c("cnop_till::CNOP.mgt|change == rel|op = c(6)|luse != URBM,FRST,FRSD|subbasin >= 16",
        "cnop_hrvst::CNOP.mgt|change=rel|op=5|subbasin = 1:15",
        "cnop_plant::CNOP.mgt|change=rel|op=5|subbasin=1:15")
+bool_op <- "\\=|\\=\\=|\\!\\=|\\<|\\<\\=|\\>|\\>\\="
 
 model_par <- tibble(par_name  = gsub("\\:\\:.*", "", a) %>% trimws(.),
                     parameter = gsub(".*\\:\\:|\\|.*","", a) %>% trimws(.)) %>%
@@ -17,7 +18,6 @@ filter_expr <- a %>%
   strsplit(., "\\|") %>%
   map(., ~ .x[2:length(.x)] %>% .[!grepl("change",.)])
 
-bool_op <- "\\=|\\=\\=|\\!\\=|\\<|\\<\\=|\\>|\\>\\="
 
 filter_var <- map(filter_expr, ~ gsub("["%&%bool_op%&%"].*", "", .x)) %>%
   map(., ~ trimws(.x)) %>%
@@ -30,6 +30,8 @@ filter_op  <- map(filter_expr, ~ gsub("[^"%&%bool_op%&%"]", "", .x)) %>%
   map(., ~ gsub("\\=", "\\=\\=", .x )) %>%
   map(., ~ substr(.x, 1, 2))
 
+expressions <- pmap_chr(list(filter_var, filter_val, filter_op), build_expr)
+
 concat_values <- function(x){
   x[!grepl("c\\((.*?)\\)",x) & grepl("\\,", x)] <-
     "c("%&%x[!grepl("c\\((.*?)\\)",x) & grepl("\\,", x)]%&%")"
@@ -37,13 +39,22 @@ concat_values <- function(x){
 }
 
 build_expr <- function(var, val, op) {
+  pmap_chr(list(var, val, op), build_filter) %>%
+    paste(., collapse = " %>% ")
+}
+
+build_filter <- function(var, val, op) {
   if(grepl("\\,|\\:", val)){
     if(op == "==") {
       expr <- "filter(.,"%&&%var%&&%"%in%"%&&%val%&%")"
     } else if (op == "!=") {
       expr <- "filter(., !("%&%var%&&%"%in%"%&&%val%&%"))"
     } else {
-      stop("...")
+      stop("Operator"%&&%op%&&%"not valid together with multiple filter values.")
     }
+  } else {
+    expr <- "filter(.,"%&&%var%&&%op%&&%val%&%")"
   }
+  return(expr)
 }
+
