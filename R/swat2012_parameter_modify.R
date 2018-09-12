cedc406832d28a0d2beb5a635ecaeedbbd03500c
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-09-12 11:08:26 -0500

    parameter rewriting for linear files works

--- R/swat2012_parameter_modify.R
+++ R/swat2012_parameter_modify.R
@@ -4,16 +4,35 @@ thread_parameter <- swat_parameter
 parameter$parameter_constrain
 
 
-
-update_par <- function(par, par_up, change){
-  case_when(
+update_par <- function(par, par_up, change, to_chg){
+  par[to_chg] <- case_when(
     change == "rep" ~ par_up,
-    change == "rel" ~ par * (1 + par_up),
-    change == "abs" ~ par_old + par_up
+    change == "rel" ~ par[to_chg] * (1 + par_up),
+    change == "abs" ~ par[to_chg] + par_up
     )
+  return(par)
 }
 
+i_run <- 1
+i_par <- 3
 
+file_i     <- parameter$parameter_constrain$file_name[i_par]
+par_i      <- parameter$parameter_constrain$par_name[i_par]
+swat_par_i <- parameter$parameter_constrain$parameter[i_par]
+change_i   <- parameter$parameter_constrain$change[i_par]
+to_chg_i   <- parameter$parameter_constrain$file_expression[i_par] %>%
+  evaluate_expression(file_meta, .) %>%
+  .[["file_code"]] %>%
+  is_in_file_codes(., thread_parameter[[file_i]][["value"]][["file_code"]])
+par_up_i   <- parameter$values[[par_i]][i_run]
+
+is_in_file_codes <- function(code_chg, code_file) {
+  code_file %in% code_chg
+}
+
+thread_parameter[[file_i]][["value"]][[swat_par_i]] <-
+  thread_parameter[[file_i]][["value"]][[swat_par_i]] %>%
+    update_par(., par_up_i, change_i, to_chg_i)
 
 par_new <- backup
 
