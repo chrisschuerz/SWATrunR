97a13a9a663019d70314eb494cc8fab20f036ded
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-09-13 17:16:55 -0500

    implemented mgt lookup table

--- R/swat2012_parameter_modify.R
+++ R/swat2012_parameter_modify.R
@@ -17,7 +17,10 @@ for (i_par in 1:ncol(parameter$values)) {
 
   if(file_i == "mgt") {
     swat_par_i <- parameter$parameter_constrain$parameter[i_par]
-    if(swat_par_i %in% names(thread_parameter$mgt$value)){
+    gen_mgt_par <- c("IGRO", "PHU_PLT", "BIOMIX", "CN2", "USLE_P", "BIO_MIN",
+                     "FILTERW", "IURBAN", "URBLU", "IRRSC", "IRRNO", "FLOWMIN",
+                     "DIVMAX", "FLOWFR", "DDRAIN", "TDRAIN", "GDRAIN")
+    if(swat_par_i %in% gen_mgt_par){
       thread_parameter <-
         modify_par(parameter, thread_parameter, file_meta, i_par, i_run)
     } else {
@@ -51,6 +54,48 @@ modify_par <- function(parameter, model_parameter, file_meta,
 }
 
 modify_mgt_par <- function() {
+  mgt_lookup <- tribble(
+    ~par,           ~mgt_op,     ~mgt_i,
+    "PLANT_ID",     1,           "MGT1",
+    "CURYR_MAT",    1,           "MGT3",
+    "HEAT_UNITS",   1,           "MGT4",
+    "LAI_INIT",     1,           "MGT5",
+    "BIO_INIT",     1,           "MGT6",
+    "HI_TARG",      1,           "MGT7",
+    "BIO_TARG",     1,           "MGT8",
+    "CNOP_TILL",    1,           "MGT9",
+    "IRR_AMT",      2,           "MGT4",
+    "FERT_ID",      3,           "MGT1",
+    "FRT_KG",       3,           "MGT4",
+    "FRT_SURFACE",  3,           "MGT5",
+    "PEST_ID",      4,           "MGT1",
+    "PST_KG",       4,           "MGT4",
+    "CNOP_HRVST",   5,           "MGT4",
+    "TILL_ID",      6,           "MGT1",
+    "CNOP_TILL",    6,           "MGT4",
+    "HARVEFF",      7,           "MGT4",
+    "HI_OVR",       7,           "MGT5",
+    "GRZ_DAYS",     9,           "MGT1",
+    "MANURE_ID",    9,           "MGT2",
+    "BIO_EAT",      9,           "MGT4",
+    "BIO_TRMP",     9,           "MGT5",
+    "MANURE_KG",    9,           "MGT6",
+    "WSTRS_ID",     10,          "MGT1",
+    "AUTO_WSTRS",   10,          "MGT4",
+    "AFERT_ID",     11,          "MGT1",
+    "AUTO_NSTRS",   11,          "MGT4",
+    "AUTO_NAPP",    11,          "MGT5",
+    "AUTO_NYR",     11,          "MGT6",
+    "AUTO_EFF",     11,          "MGT7",
+    "AFRT_SURFACE", 11,          "MGT8",
+    "SWEEPEFF",     12,          "MGT4",
+    "FR_CURB",      12,          "MGT5",
+    "IMP_TRIG",     13,          "MGT1",
+    "FERT_DAYS",    14,          "MGT1",
+    "CFRT_ID",      14,          "MGT2",
+    "IFRT_FREQ",    14,          "MGT3",
+    "CFRT_KG",      14,          "MGT4")
+
 
 }
 
