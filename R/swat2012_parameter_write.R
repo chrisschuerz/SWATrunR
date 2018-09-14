cef83cdf77c7b7e9ceb90b99b085378747608051
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-09-14 14:22:18 -0500

    chm and sol writing implemented

--- R/swat2012_parameter_write.R
+++ R/swat2012_parameter_write.R
@@ -20,40 +20,58 @@ write_par_list <- function(file_meta, file_suffix,
   walk2(par_files, file_sel$file, ~ write_lines(.x, thread_path%//%.y))
 }
 
-write_chm <- function(file_meta, file_suffix,
-                      thread_parameter, thread_path) {
-  file_sel <- filter(file_meta, file_name == file_suffix)
-
-  map(file_sel$file_code, ~ filter(swat_parameter$chm$value, file_code == .x)) %>%
-    map(., ~ select(.x, -file_code, -idx)) %>%
-    map(., ~map_df(.x, ~sprintf("%16s", .x))) %>%
-    map(., ~t(.x))
-
-  table_pos <-  3:8
-}
-
-read_chm <- function(file_meta, project_path) {
+write_chm <- function(file_meta, thread_parameter, thread_path) {
   file_sel <- filter(file_meta, file_name == "chm")
-
-  files <- map(project_path%//%file_sel$file, read_lines)
-
   table_pos <-  3:8
-  col_pos   <-  c(28 + (0:10)*rep(12))
-  par_name  <-  c("LAYER","SOL_NO3", "SOL_ORGN",
-                  "SOL_LABP", "SOL_SOLP", "SOL_ORGP")
+  line_name <- thread_parameter$chm$file[[1]][table_pos] %>%
+    gsub("\\:.*", "", .) %>%
+    paste0(., ":")
 
-  par_table <- map(files, ~ get_table(.x, table_pos, col_pos)) %>%
-    map(., ~ set_names(.x, par_name)) %>%
-    map2(., file_sel$file_code, ~ mutate(.x, file_code = .y)) %>%
-    bind_rows(.) %>%
-    mutate(idx = 1:nrow(.))
-
-  return(list(file = files, value = par_table))
+  par_files <-  map(file_sel$file_code,
+                  ~ filter(thread_parameter$chm$value, file_code == .x)) %>%
+    map(., ~ select(.x, -file_code, -idx)) %>%
+    map(., ~map_df(.x, ~sprintf("%12s", .x))) %>%
+    # map(., ~t(.x)) %>%
+    map(., ~map(.x, ~paste0(.x, collapse = ""))) %>%
+    map(., ~unlist(.x)) %>%
+    map(., ~paste(line_name, .x)) %>%
+    map2(., thread_parameter$chm$file, function(x, y, pos){y[pos] <- x
+                                         return(y)}, table_pos)
+  walk2(par_files, file_sel$file, ~ write_lines(.x, thread_path%//%.y))
 }
 
-get_table <- function(file_i, table_pos, col_pos) {
-  col_start <- col_pos[1:(length(col_pos)-1)]
-  col_end   <- col_pos[2:length(col_pos)] - 1
-  file_i[table_pos] %>%
-    map_dfc(., ~ split_line(.x, col_start, col_end))
+write_sol <- function(file_meta, thread_parameter, thread_path) {
+  file_sel <- filter(file_meta, file_name == "sol")
+  par_pos <- 4:6
+  table_pos <-  8:21
+  par_name <- thread_parameter$sol$file[[1]][par_pos] %>%
+    gsub("\\:.*", "", .) %>%
+    paste0(., ":")
+  table_name <- thread_parameter$sol$file[[1]][table_pos] %>%
+    gsub("\\:.*", "", .) %>%
+    paste0(., ":")
+
+  par_file <- map(file_sel$file_code,
+                    ~ filter(thread_parameter$sol$value, file_code == .x)) %>%
+    map(., ~.x[,1:14]) %>%
+    map(., ~map_df(.x, ~sprintf("%12s", .x))) %>%
+    map(., ~map(.x, ~paste0(.x, collapse = ""))) %>%
+    map(., ~unlist(.x)) %>%
+    map(., ~paste(table_name, .x)) %>%
+    map2(., thread_parameter$sol$file, function(x, y, pos){y[pos] <- x
+    return(y)}, table_pos)
+
+  par_file <- map(file_sel$file_code,
+      ~ filter(thread_parameter$sol$value, file_code == .x)) %>%
+    map(., ~ select(.x, SOL_ZMX, ANION_EXCL, SOL_CRK) %>%
+          .[1,] %>%
+          as.character(.)) %>%
+    map(., ~paste(par_name, .x)) %>%
+    map2(., par_file, function(x, y, pos){y[pos] <- x
+    return(y)}, par_pos)
+
+  walk2(par_file, file_sel$file, ~ write_lines(.x, thread_path%//%.y))
 }
+
+write_mgt <- function(file_meta, thread_parameter, thread_path) {
+  file_sel <- filter(file_meta, file_name == "sol")
