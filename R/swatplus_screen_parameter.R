3002e3a49dcf23171a144950347b5e7df0eb89c5
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-09-28 23:06:46 +0200

    Implemented logistic regression approach according to Cuntz et al for identification of informative parameters

--- R/swatplus_screen_parameter.R
+++ R/swatplus_screen_parameter.R
@@ -1,7 +1,7 @@
 # SCREEN
 screen <- function(project_path, parameter, calibration_var,
-                   tolerance = 0.05, sc_min = 1, output = NULL,
-                   start_date = NULL, end_date = NULL,
+                   tolerance = 0.05, sc_min = 1, eet_iter_max = 3,
+                   output = NULL, start_date = NULL, end_date = NULL,
                    output_interval = NULL, years_skip = NULL,
                    run_path = NULL, n_thread = NULL) {
 
@@ -13,6 +13,11 @@ screen <- function(project_path, parameter, calibration_var,
     return_output <- TRUE
   }
 
+  cal_names <- names(calibration_var)
+  cal_output <- map(cal_names, ~ define_output(file = "waterbal_aa_bsn.txt",
+                                               variable = .x,
+                                               unit = 1)) %>%
+    set_names(cal_names)
   ### Defeine the outputs for soft calibration here also with define output to
   ### then use the read output function from the R package
 
@@ -27,16 +32,114 @@ screen <- function(project_path, parameter, calibration_var,
 
   while (sc_found <= sc_min) {
     sim <- run_swatplus(project_path = project_path, output = output,
+                        parameter = par_samp,
                         start_date = start_date, end_date = end_date,
                         output_interval = output_interval,
                         years_skip = years_skip, run_path = run_path,
                         n_thread = n_thread, keep_folder = TRUE, quiet = FALSE,
                         return_output = return_output, soft_calibration = TRUE)
 
+    if(is.null(run_path)){
+      cal_path <- project_path%//%".model_run"
+    } else {
+      cal_path <- run_path%//%".model_run"
+    }
+    threads <- dir(cal_path) %>% .[grepl("thread_", .)]
+
+    cal_sim <- map(threads, ~ read_swatplus_output(cal_output,
+                                                   cal_path%//%.x)) %>%
+      map(., ~ extract_output(cal_output, .x)) %>%
+      bind_rows()
 
   }
 
 
+}
+
+#' Find threshold between informative and non-informative parameters based on
+#' their sensitivities
+#'
+#' @param sens_value Numeric vector of parameter sensitivities
+#'
+#' @importFrom dplyr %>%
+#'
+#' @keywords internal
+#'
+find_sensitivity_threshold <- function(sens_value) {
+  sens_norm <- (sens_value - min(sens_value))/
+    (max(sens_value) - min(sens_value)) %>%
+    sort(.)
+  x <- seq(0, 1, length.out = length(sens_norm))
+
+  logi_opt <- optim(par = c(0,1,1,0), sse, x = x, y = sens_norm, fun = logi)
+
+  x_est <- seq(0,1,length.out = 100*length(sens_norm))
+  k <- logi_curv(x_est, res$par)
+  max_curv <- which.max(k)
+  thres_norm <- logi(x_est, logi_opt$par)[max_curv]
+  sens_thres <- (max(sens_value) - min(sens_value)) * thres_norm + min(sens_value)
+
+  return(sens_thres)
+}
+
+#' Logistic regression function with offset
+#'
+#' @param x Numeric vector of x values
+#' @param par Numeric parameter set, providing (1) offset, (2) Curve's max
+#'   point, (3) steepness, and (4) x value of sigmoid mid-point
+#'
+#' @keywords internal
+#'
+logi <- function(x, par) {par[1] + par[2]/(1 + exp(-par[3]*(x - par[4])))}
+
+#' First derivative of the logistic regression function
+#'
+#' @param x Numeric vector of x values
+#' @param par Numeric parameter set, providing (1) offset, (2) Curve's max
+#'   point, (3) steepness, and (4) x value of sigmoid mid-point
+#'
+#' @keywords internal
+#'
+logi_drv1 <- function(x, par) {
+  par[2]*par[3]/ (2*cosh(par[3]*(x - par[4])) + 1)
+}
+
+#' Second derivative of the logistic regression function
+#'
+#' @param x Numeric vector of x values
+#' @param par Numeric parameter set, providing (1) offset, (2) Curve's max
+#'   point, (3) steepness, and (4) x value of sigmoid mid-point
+#'
+#' @keywords internal
+#'
+logi_drv2 <- function(x, par) {
+  -par[3]^2*par[2]*sinh(par[3]*(x - par[4]))/
+    (2*cosh(par[3]*(x - par[4])) + 1)^2
+}
+
+#' Curvature of the logistic regression function
+#'
+#' @param x Numeric vector of x values
+#' @param par Numeric parameter set, providing (1) offset, (2) Curve's max
+#'   point, (3) steepness, and (4) x value of sigmoid mid-point
+#'
+#' @keywords internal
+#'
+logi_curv <- function(x, par) {
+  l_reg_drv2(x, par)/(1 + l_reg_drv1(x, par))^(3/2)
+}
+
+#' Sum of squared errors
+#'
+#' @param x Numeric vector of x values
+#' @param y Numeric vector of 'observed' y values
+#' @param par Numeric vector of parameter set
+#' @param fun Model function to evaluate
+#'
+#' @keywords internal
+#'
+sse <- function(x, y, par, fun) {
+  sum((y - fun(x, par))^2)
 }
 
 
