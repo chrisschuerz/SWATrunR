dcaefca8c919bcf73abc60ca6bd7e683ca9331be
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-10-10 22:29:04 +0200

    minor tests

--- demo/swat_plus_test.R
+++ demo/swat_plus_test.R
@@ -1,18 +1,18 @@
 para <- tibble::tibble("cn2.hru|change = pctchg" = runif(8,-5,5),
                "alpha.gw|change = absval" = runif(8, 0, 1))
-pro_pth <- "C:/180927_TxtInOut_2aqu"
+pro_pth <- "D:/SWATplus/TxtInOut_2aqu"
 
 aa <- run_swatplus(project_path = pro_pth,
                    output = list(q =  define_output(file = "channel",
-                                                    variable = "floout",
+                                                    variable = "flo_out",
                                                     unit = 18),
-                                 et = define_output(file = "basin_wb",
+                                 et = define_output(file = "lsunit_wb",
                                                     variable = "et",
-                                                    unit = 1)),
+                                                    unit = 1:20)),
                    parameter = para,
-                   start_date = "2007-03-01",
+                   start_date = "2007-01-01",
                    end_date = "2009-12-31",
                    output_interval = "d",
                    years_skip = 1,
-                   n_thread = 4,
-                   save_file = "test")
+                   n_thread = 4, keep_folder = T,
+                   save_file = "test_lsu")
