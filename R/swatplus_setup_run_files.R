2f8d6cff83c7ff9b93759c0497d2db60ec00cfb6
chrisschuerz <christoph.schuerz@boku.ac.at>
2021-06-22 12:11:18 +0200

    Implament a set of functions to include parameter conditions in SWAT+ calibration.cal

--- R/swatplus_setup_run_files.R
+++ R/swatplus_setup_run_files.R
@@ -23,7 +23,7 @@
 setup_swatplus <- function(project_path, parameter, output,
                            start_date, end_date,
                            output_interval, years_skip,
-                           soft_cal) {
+                           soft_cal, unit_cons) {
   ## Read unmodified time.sim, calibration.cal and print.prt
   options(readr.num_columns = 0)
   model_setup <- list()
@@ -140,15 +140,169 @@ setup_swatplus <- function(project_path, parameter, output,
 
   # Current implementation of parameter calibration does not allow any constraints!
   if(!is.null(parameter)) {
-    model_setup$calibration.cal <- parameter$definition %>%
-      select(., parameter, change) %>%
-      set_names(c("NAME", "CHG_TYPE")) %>%
-      mutate(VAL = NA, CONDS = 0, LYR1 = 0, LYR2 = 0, YEAR1 = 0, YEAR2 = 0,
-             DAY1 = 0, DAY2 = 0, OBJ_TOT = 0)
+    model_setup$calibration.cal <- map(1:nrow(parameter$definition), ~ parameter$definition[.x,]) %>%
+      map(., ~ setup_calibration_cal(.x, unit_cons)) %>%
+      bind_rows(.)
   }
 
   return(model_setup)
 }
+
+setup_calibration_cal <- function(par_def, unit_cons) {
+  par_cal <- init_cal(par_def)
+
+  if('lyr' %in% names(par_def)) {
+    if(!is.na(par_def$lyr)) {
+      par_cal <- add_value_range(par_cal, par_def$lyr, 'LYR')
+    }
+  }
+  if('year' %in% names(par_def)) {
+    if(!is.na(par_def$year)) {
+      par_cal <- add_value_range(par_cal, par_def$year, 'YEAR')
+    }
+  }
+  if('day' %in% names(par_def)) {
+    if(!is.na(par_def$day)) {
+      par_cal <- add_value_range(par_cal, par_def$day, 'DAY')
+    }
+  }
+  if('unit' %in% names(par_def)) {
+    if(!is.na(par_def$unit)) {
+      par_cal <- add_obj(par_cal, par_def$unit,
+                         unit_cons$units[[par_def$file_name]])
+    }
+  }
+
+  if(any(c('hsg', 'texture', 'plant', 'landuse') %in% names(par_def))) {
+      soil_luse <- select(par_def, any_of(c('hsg', 'texture', 'plant', 'landuse'))) %>%
+        select(., !where(is.na)) %>%
+        map(., ~ .x)
+      cond_tbl <- map2_df(soil_luse, names(soil_luse), ~ add_soil_luse(.x, .y, unit_cons$conds))
+  }
+
+  if(any('slope' %in% names(par_def))) {
+    if(!is.na(par_def$slope)) {
+
+      cond_tbl <- add_slope(par_def$slope, cond_tbl)
+    }
+  }
+
+  if(any(c('hsg', 'texture', 'plant', 'landuse') %in% names(par_def))) {
+    par_cal$CONDS <- as.character(nrow(cond_tbl))
+    par_cal <- bind_rows(par_cal, cond_tbl)
+  }
+
+  return(par_cal)
+}
+
+init_cal <- function(par_def) {
+  par_def %>%
+  select(., parameter, change) %>%
+    set_names(c("NAME", "CHG_TYPE")) %>%
+    mutate(VAL = NA_real_, CONDS = '0', LYR1 = 0, LYR2 = 0, YEAR1 = 0, YEAR2 = 0,
+           DAY1 = 0, DAY2 = 0, OBJ_TOT = 0)
+}
+
+add_value_range <- function(par_cal, val, var) {
+  val_range <- get_value_range(val)
+  par_cal[var%&%'1'] <- val_range[1]
+  par_cal[var%&%'2'] <- val_range[2]
+  return(par_cal)
+}
+
+# add_year <- function(par_cal, yr) {
+#   yr_range <- get_value_range(yr)
+#   par_cal$YEAR1 <- yr_range[1]
+#   par_cal$YEAR2 <- yr_range[2]
+#   return(par_cal)
+# }
+#
+# add_day <- function(par_cal, day) {
+#   day_range <- get_value_range(day)
+#   par_cal$DAY1 <- day_range[1]
+#   par_cal$DAY2 <- day_range[2]
+#   return(par_cal)
+# }
+
+add_obj <- function(par_cal, unit, unit_all) {
+  eval_unit <- paste('unit_all', unit) %>%
+  parse(text = .) %>%
+  eval(.)
+
+  unit <- unit_all[eval_unit] %>%
+    unique(.) %>%
+    sort(.) %>%
+    identify_sequence(.) %>%
+    set_names(., 'OBJ'%_%1:length(.)) %>%
+    bind_rows()
+
+  par_cal$OBJ_TOT <- length(unit)
+  par_cal <- bind_cols(par_cal, unit)
+
+  return(par_cal)
+}
+
+add_soil_luse <- function(cond, var, cond_all) {
+  eval_cond <- paste('cond_all[[var]]', cond) %>%
+    parse(text = .) %>%
+    eval(.)
+
+  cond <- cond_all[[var]][eval_cond]
+  tbl <- tibble(NAME = var,
+                CHG_TYPE = '=',
+                VAL = 0,
+                CONDS = cond)
+  return(tbl)
+}
+
+add_slope <- function(cond, cond_tbl) {
+  cond_op  <- str_extract(cond, '<\\=|\\>\\=|\\=\\=|\\=|\\<|\\>')
+  cond_val <- str_remove(cond, cond_op) %>% str_trim(.) %>% as.numeric()
+  if(is.na(cond_val)) {
+    stop("Parameter condition 'slope ", cond, "' is not allowed.")
+  }
+
+  slp_tbl <- tibble(NAME = 'slope',
+                    CHG_TYPE = str_sub(cond_op, 1, 1),
+                    VAL = cond_val)
+  cond_tbl <- bind_rows(cond_tbl, slp_tbl)
+
+  return(cond_tbl)
+}
+
+get_value_range <- function(condition) {
+  if(str_detect(condition, '%in%')) {
+    condition <- condition %>%
+      str_remove(., '%in%') %>%
+      parse(text = .) %>%
+      eval(.)
+  } else if (str_detect(condition, '==')) {
+    condition <- condition %>%
+      str_remove(., '==') %>%
+      as.numeric(.)
+  } else {
+    stop("For parameter conditioning with 'lyr', 'year', and 'day' only single",
+         "values or upper lower bound implemented yet!")
+  }
+  return(c(min(condition), max(condition)))
+}
+
+identify_sequence <- function(val) {
+  split_end <- which(diff(val) > 1)
+  split_start <- c(1, split_end + 1)
+  split_end <- c(split_end, length(val))
+  map2(split_start, split_end, ~ val[.x:.y]) %>%
+  map(., ~ translate_sequence(.x)) %>%
+  reduce(., c)
+}
+
+translate_sequence <- function(val_seq) {
+  if (length(val_seq) > 1) {
+    val_seq <- c(min(val_seq), -max(val_seq))
+  }
+  return(val_seq)
+}
+
 
 #' Write the updated file.cio to all parallel folders
 #'
