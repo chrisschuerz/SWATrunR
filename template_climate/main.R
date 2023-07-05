59a307f8b06a49b02863704287c7572a808101db
biopsichas <svajunas.plunge@gmail.com>
2023-07-05 17:03:42 +0300

    cliamte template started

---
+++ template_climate/main.R
@@ -0,0 +1,20 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(SWATrunR)
+library(tidyverse)
+
+n_thread <- 2
+
+test_path <-"../test/TxtInOut"
+climate_set <- list.dirs(path = "../test/cli3", full.names = TRUE, recursive = TRUE) %>%
+  .[grepl('N|E|H',.)]
+#
+# debug(run_swatplus)
+# options(error=recover)
+res <- run_swatplus(project_path = test_path,
+                    output = list(q = define_output('channel_sd', 'flo_out', 52),
+                                  et = define_output("basin_wb", "et", 1)),
+                    climate_set = climate_set,
+                    output_interval = "y",
+                    years_skip = 3,
+                    n_thread = n_thread,
+                    keep_folder = TRUE)
