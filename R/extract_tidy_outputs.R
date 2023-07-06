ba0eacc4b368b8516e01fa079dc2282bf729f5a6
Christoph Schürz <christoph.schuerz@ufz.de>
2023-07-06 09:43:18 +0200

    Strongly revise output restructuring
    - Outputs have only one structure independent of the number of runs
      or if parameters were changed
    - Structure now:
      * All simulations are in .$simulation$<var_name>
      * Parameters are always in .$parameter if changes were applied
      * Error is always in .$error_report if errors occured

--- R/extract_tidy_outputs.R
+++ R/extract_tidy_outputs.R
@@ -53,78 +53,125 @@ extract_out_i <- function(out_i, mod_out) {
 #' @importFrom tibble add_column as_tibble enframe
 #' @keywords internal
 #'
-tidy_results <- function(sim_result, parameter, date, add_parameter,
+tidy_results <- function(sim_result, parameter, add_parameter,
                          add_date, run) {
+  n_digit <- length(sim_result) %>% as.character(.) %>% nchar(.)
+  sim_result <- set_names(sim_result, "run"%_%sprintf("%0"%&%n_digit%&%"d", run))
 
-  if(length(sim_result) == 1) {
-    sim_result <- sim_result[[1]]
-    is_result <- is.data.frame(sim_result)
-    if(!is_result) {
-      error_report <- sim_result
-      sim_result <- NULL
-    } else {
-      error_report <- NULL
-    }
+  is_result <- map_lgl(sim_result, is.list)
+  if(!all(is_result)) {
+    error_report <- sim_result[!is_result] %>%
+      enframe() %>%
+      set_names(c("run", "message")) %>%
+      add_column(. ,error = map_chr(.$message, ~.x[which(.x == 'Error:') + 1]), .after = "run") %>%
+      add_column(. ,idx = as.numeric(str_remove(.$run, "run_")), .before = 1)
+
+    sim_result <- sim_result[is_result]
   } else {
-    if(!is.null(parameter)) {
-      n_digit <- get_digit(parameter$values)
-    } else {
-      n_digit <- length(sim_result) %>% as.character(.) %>% nchar(.)
-    }
-
-    is_run_name <- max(nchar(str_extract(names(sim_result[1]), 'run_')), 0) > 0
-    if(!is_run_name) {
-      sim_result <- set_names(sim_result, "run"%_%sprintf("%0"%&%n_digit%&%"d", run))
-    }
-
-    is_result <- map_lgl(sim_result, is.list)
-
-    if(!all(is_result)) {
-      error_report <- sim_result[!is_result] %>%
-        enframe() %>%
-        set_names(c("run", "message"))
-      error_report <- error_report %>%
-        add_column(. ,error = map_chr(.$message, ~.x[which(.x == 'Error:') + 1]), .after = "run") %>%
-        add_column(. ,idx = as.numeric(str_remove(.$run, "run_")), .before = 1)
-    } else {
-      error_report <- NULL
-    }
-    if(!all(!is_result)) {
-      sim_result <- sim_result[is_result] %>%
-        transpose(.) %>%
-        map(., ~ as_tibble(.x))
-    } else {
-      sim_result <- NULL
-    }
+    error_report <- NULL
   }
 
-  if(add_date & !is.null(sim_result)) {
-    sim_date <- date
-    if(is.data.frame(sim_result)){
-      sim_result <- bind_cols(sim_date, sim_result)
-    } else {
-      sim_result <- map(sim_result, ~ bind_cols(sim_date, .x))
-    }
+  if(length(sim_result) > 0) {
+    add_cols <- map(sim_result[[1]], ~ extract_non_var_cols(.x))
+    var_cols <- map(sim_result, ~ map(.x, ~ remove_non_var_cols(.x)))
+
+    var_add_assgn <- map2(var_cols[[1]],
+                          1:length(var_cols[[1]]),
+                          ~ rep(.y, ncol(.x))) %>%
+      list_c(.)
+
+    var_cols <- var_cols %>%
+      map(., ~ map(.x, ~ map(.x, ~.x))) %>%
+      map(., ~ list_flatten(.x)) %>%
+      list_transpose(., simplify = FALSE) %>%
+      map(., ~ bind_cols(.x))
+
+    add_cols <- add_cols[var_add_assgn]
+
+    sim_result <- map2(add_cols, var_cols, ~bind_cols(.x, .y)) %>%
+      set_names(names(var_cols))
+
+
+  } else {
+    sim_result <- NULL
   }
+
+  output_list <- list()
 
   if(add_parameter & !is.null(parameter$value)) {
-    sim_result <- list(parameter  = parameter,
-                       simulation = sim_result)
+    output_list$parameter <- parameter
   }
+
+  output_list$simulation <- sim_result
 
   if(!is.null(error_report)) {
-    if(add_parameter & !is.null(parameter$value)) {
-      sim_result$error_report <- error_report
-    } else if (is.null(sim_result) & is.character(error_report)) {
-      sim_result <- error_report
-    } else {
-      sim_result <- list(simulation = sim_result,
-                         error_report = error_report)
-    }
+    output_list$error_report <- error_report
   }
 
-  return(sim_result)
+  return(output_list)
 }
+
+
+extract_non_var_cols <- function(tbl) {
+  if (names(tbl)[1] == 'date') {
+    col_extr <- tbl[1]
+  } else if(all(names(tbl)[c(1,2)] == c('year', 'plant_name'))) {
+    col_extr <- tbl[1:2]
+  } else if (names(tbl)[1] == 'p' & all(tbl[[1]][1:3] == c(0, 0.1, 0.5))) {
+    col_extr <- tbl[1]
+  } else {
+    col_extr <- NULL
+  }
+  return(col_extr)
+}
+
+remove_non_var_cols <- function(tbl) {
+  if (names(tbl)[1] == 'date') {
+    tbl <- tbl[2:ncol(tbl)]
+  } else if(all(names(tbl)[c(1,2)] == c('year', 'plant_name'))) {
+    tbl <- tbl[3:ncol(tbl)]
+  } else if (names(tbl)[1] == 'p' & all(tbl[[1]][1:3] == c(0, 0.1, 0.5))) {
+    tbl <- tbl[2:ncol(tbl)]
+  }
+  return(tbl)
+}
+
+  # if(length(sim_result) == 1) {
+  #   sim_result <- sim_result[[1]]
+  #   is_result <- is.data.frame(sim_result)
+  #   if(!is_result) {
+  #     error_report <- sim_result
+  #     sim_result <- NULL
+  #   } else {
+  #     error_report <- NULL
+  #   }
+  # } else {
+  #   if(!is.null(parameter)) {
+  #     n_digit <- get_digit(parameter$values)
+  #   } else {
+  #     n_digit <- length(sim_result) %>% as.character(.) %>% nchar(.)
+  #   }
+
+
+    # is_run_name <- max(nchar(str_extract(names(sim_result[1]), 'run_')), 0) > 0
+
+
+
+
+
+
+
+  # if(add_date & !is.null(sim_result)) {
+  #   sim_date <- date
+  #   if(is.data.frame(sim_result)){
+  #     sim_result <- bind_cols(sim_date, sim_result)
+  #   } else {
+  #     sim_result <- map(sim_result, ~ bind_cols(sim_date, .x))
+  #   }
+  # }
+
+
+
 
 #' Create date vector from the date info in the model setup of a SWAT2012 project
 #'
